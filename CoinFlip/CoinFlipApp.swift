8e566afa0877c958711de10aac7ef61bba1c6018
RTBlanco <ronnytoribio1@hotmail.com>
2021-09-13 11:07:28 -0400

    Initial Commit

---
+++ CoinFlip/CoinFlipApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinFlipApp.swift
+//  CoinFlip
+//
+//  Created by Ronny Toribio on 9/13/21.
+//
+
+import SwiftUI
+
+@main
+struct CoinFlipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
